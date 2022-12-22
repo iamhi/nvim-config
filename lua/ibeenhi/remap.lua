b27381651d53c0b3f920b8bc8c0c5a7bbba5806a
iamhi <inpactt@gmail.com>
2022-12-22 02:49:39 +0100

    Cursor fix

--- lua/ibeenhi/remap.lua
+++ lua/ibeenhi/remap.lua
@@ -4,6 +4,6 @@ local nnoremap = require("ibeenhi.keymap").nnoremap
 -- nnoremap("<leader>dd", "<cmd>Lexplore %:p:h<CR>")
 -- nnoremap("<C-l>",":CtrlSF ")
 nnoremap('<C-s>', ':w<CR>')
-vim.keymap.set('i', '<C-s>', '<Esc>:w<CR>a')
+vim.keymap.set('i', '<C-s>', '<Esc>:w<CR>')
 
 vim.keymap.set('v', '<C-c>', 'y :call system(\'xclip\', @0)<CR>')
